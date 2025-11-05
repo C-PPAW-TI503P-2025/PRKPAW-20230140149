b90f4bb60abcb69ec773332c39e1263700d91383
annisadiann <161261950+annisadiann@users.noreply.github.com>
2025-11-05 23:07:41 +0700

    Delete routes directory

--- routes/presensi.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const presensiController = require('../controllers/presensiController.js');
-const { addUserData } = require('../middleware/permissionMiddleware.js');
-router.use(addUserData);
-router.post('/check-in', presensiController.CheckIn);
-router.post('/check-out', presensiController.CheckOut);
-module.exports = router;
